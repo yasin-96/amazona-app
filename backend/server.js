12a3f297dcf6a788e75cdda8954cad2c2faa222f
Yasin Eraslan <yasin.eraslan@mni.thm.de>
2021-02-22 16:02:18 +0100

    Implemented mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,28 @@
-import express from 'express';
-import data from './data.js';
+import express from "express";
+import mongoose from "mongoose";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
+mongoose.connect(
+  "mongodb+srv://yasin:cimbom21@cluster0.f8enl.mongodb.net/test?authSource=admin&replicaSet=atlas-abz77t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
   }
-});
+);
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
